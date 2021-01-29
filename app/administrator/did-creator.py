d170f0f2158b59557cb9ac017e8ceebff6c98b39
sfl0r3nz05 <santiagofigueroalorenzo@gmail.com>
2021-01-29 20:18:50 +0000

    29.01.21.2

--- app/administrator/did-creator.py
+++ app/administrator/did-creator.py
@@ -34,11 +34,13 @@ function = 'controllerRegister'
 registerController(net_profile, organization, user, channel,
                    peer, chaincode, function, did, payload)
 
-# 4. Register client (No irá en este archivo, lo hace el client)
+# 4. Generar payload para registrar el cliente
 pathprivKey = '/home/santiago/ModbusSSIHFB/crypto-material/config_solo/crypto-config/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/keystore/da72fd6c0f4595d33eb9ae6f6d06cd171ebc3882fc856960c244b9b5c2b35a90_sk'
-pathpubKey = '/home/santiago/ModbusSSIHFB/crypto-material/config_solo/crypto-config/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/admincerts/User1@org1.example.com-cert.pem'
+did_client, public_key_client = payloadToRegisterEntity(pathprivKey)
+print(did_client)
+print(public_key_client)
 
-
+# 5. Register client (No irá en este archivo, lo hace el client)
 net_profile = '../../connection-profile/2org_2peer_solo/network.json'
 organization = 'org1.example.com'
 user = 'Admin'
@@ -47,9 +49,15 @@ peer = 'peer0.org1.example.com'
 chaincode = 'proxy_cc'
 function = 'entityRegister'
 registerEntity(net_profile, organization, user, channel,
-               peer, chaincode, function, 'did:vtn:trustid:beaf4dafac54331621dd6c685248f9e26ee45703a697f571b4d34562bd7c73fb', 'did:vtn:trustid:d7104427989de1fa5729c68f8cb767bb0740ecd65b3b080e9a725a04297f9641', '-----BEGIN CERTIFICATE-----MIICKjCCAdGgAwIBAgIRAOuqO4gIp7Z0qK+nP2m/w0UwCgYIKoZIzj0EAwIwczELMAkGA1UEBhMCVVMxEzARBgNVBAgTCkNhbGlmb3JuaWExFjAUBgNVBAcTDVNhbiBGcmFuY2lzY28xGTAXBgNVBAoTEG9yZzEuZXhhbXBsZS5jb20xHDAaBgNVBAMTE2NhLm9yZzEuZXhhbXBsZS5jb20wHhcNMTgxMDE5MDM0ODAwWhcNMjgxMDE2MDM0ODAwWjBsMQswCQYDVQQGEwJVUzETMBEGA1UECBMKQ2FsaWZvcm5pYTEWMBQGA1UEBxMNU2FuIEZyYW5jaXNjbzEPMA0GA1UECxMGY2xpZW50MR8wHQYDVQQDDBZVc2VyMUBvcmcxLmV4YW1wbGUuY29tMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEWhkliN8d3zX7cBV7gAlRGjoV7euSY759ViJZrW7vGaNDFmFnIop30KT2HT6N7UugHVK4pl1auuDQnkWlYVIRmKNNMEswDgYDVR0PAQH/BAQDAgeAMAwGA1UdEwEB/wQCMAAwKwYDVR0jBCQwIoAga6xgFWY3j82bg8aLlpkNQa21FTJkDesOhPg87B1Yl9IwCgYIKoZIzj0EAwIDRwAwRAIgXwSy7f7+ExoM1/J4bWJTybcO204klB9ry2keH6VG2ekCIHn+OF/3KxNsYSo904Df0PPBgtkZF3njS14Std3S9Mgd-----END CERTIFICATE-----')
+               peer, chaincode, function, did_client, 'did:vtn:trustid:d7104427989de1fa5729c68f8cb767bb0740ecd65b3b080e9a725a04297f9641', public_key_client)
 
-# 5. Register server (No irá en este archivo, lo hace el server)
+# 6. Generar payload para registrar el server
+pathprivKey = '/home/santiago/ModbusSSIHFB/crypto-material/config_solo/crypto-config/peerOrganizations/org2.example.com/users/User1@org2.example.com/msp/keystore/73beefad9003c589064deb2128c4f0831ba8003f1233102cc52a188afd05fe61_sk'
+did_server, public_key_server = payloadToRegisterEntity(pathprivKey)
+print(did_server)
+print(public_key_server)
+
+# 7. Register server (No irá en este archivo, lo hace el server)
 net_profile = '../../connection-profile/2org_2peer_solo/network.json'
 organization = 'org1.example.com'
 user = 'Admin'
@@ -58,16 +66,16 @@ peer = 'peer0.org2.example.com'
 chaincode = 'proxy_cc'
 function = 'entityRegister'
 registerEntity(net_profile, organization, user, channel,
-               peer, chaincode, function, 'did:vtn:trustid:4981f7c8f152f14d009c1b69d4972c84fdb4985055dc33d4d25c821ab015ad7e', 'did:vtn:trustid:d7104427989de1fa5729c68f8cb767bb0740ecd65b3b080e9a725a04297f9641', '-----BEGIN CERTIFICATE-----MIICKjCCAdCgAwIBAgIQZlZer9OVo/b4Qii45sUfEzAKBggqhkjOPQQDAjBzMQswCQYDVQQGEwJVUzETMBEGA1UECBMKQ2FsaWZvcm5pYTEWMBQGA1UEBxMNU2FuIEZyYW5jaXNjbzEZMBcGA1UEChMQb3JnMi5leGFtcGxlLmNvbTEcMBoGA1UEAxMTY2Eub3JnMi5leGFtcGxlLmNvbTAeFw0xODEwMTkwMzQ4MDBaFw0yODEwMTYwMzQ4MDBaMGwxCzAJBgNVBAYTAlVTMRMwEQYDVQQIEwpDYWxpZm9ybmlhMRYwFAYDVQQHEw1TYW4gRnJhbmNpc2NvMQ8wDQYDVQQLEwZjbGllbnQxHzAdBgNVBAMMFlVzZXIxQG9yZzIuZXhhbXBsZS5jb20wWTATBgcqhkjOPQIBBggqhkjOPQMBBwNCAASaJG8xzdJwDGoXCqouUZYHCW56Se3sU9OVj+NSGZxetfmm/+aKDqIMEd+bDPVn0HZsZYC/sYWLfwIkvI9V2/4No00wSzAOBgNVHQ8BAf8EBAMCB4AwDAYDVR0TAQH/BAIwADArBgNVHSMEJDAigCDu2ocNKJDV2HmtXnLRQ5pss5b6OW4OCYwI6HNM+hE5jDAKBggqhkjOPQQDAgNIADBFAiEA2Wsm0MmQmVpUMCr+Dxkemho26LkQl6kJaPUnGfhcBncCICuifm6WrpD/Lk80Z4VODaALfq9Wt1ZOxLx3ZWh9uEKK-----END CERTIFICATE-----')
+               peer, chaincode, function, did_server, 'did:vtn:trustid:d7104427989de1fa5729c68f8cb767bb0740ecd65b3b080e9a725a04297f9641', public_key_server)
 
-# 6. El administrador crea el didDoc for client belonging Org1 (Depende de que 4 se haya llevado a cabo)
+# 8. El administrador crea el didDoc for client belonging Org1 (Depende de que 4 se haya llevado a cabo)
 pathwallet = '/home/santiago/ModbusSSIHFB/app/modbus-sync-client/wallet.json'
 pathprivKey = '/home/santiago/ModbusSSIHFB/crypto-material/config_solo/crypto-config/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/keystore/da72fd6c0f4595d33eb9ae6f6d06cd171ebc3882fc856960c244b9b5c2b35a90_sk'
 pathpubKey = '/home/santiago/ModbusSSIHFB/crypto-material/config_solo/crypto-config/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/admincerts/User1@org1.example.com-cert.pem'
 did, controller, pubKey, payload = generateDIDentity(
     pathwallet, pathpubKey, pathprivKey, 0, 5020, 6, 1000, 0)
 
-# 7. El administrador registra el didDoc for client belonging Org1 (Depende de que 4 se haya llevado a cabo)
+# 9. El administrador registra el didDoc for client belonging Org1 (Depende de que 4 se haya llevado a cabo)
 net_profile = '../../connection-profile/2org_2peer_solo/network.json'
 organization = 'org1.example.com'
 user = 'Admin'
@@ -78,14 +86,14 @@ function = 'didDocRegister'
 registerDidDoc(net_profile, organization, user, channel,
                peer, chaincode, function, did, controller, pubKey, payload)
 
-# 8. El administrador crea el didDoc for server belonging Org1 (Depende de que 5 se haya llevado a cabo)
+# 10. El administrador crea el didDoc for server belonging Org1 (Depende de que 5 se haya llevado a cabo)
 pathwallet = '/home/santiago/ModbusSSIHFB/app/modbus-sync-server/wallet.json'
 pathprivKey = '/home/santiago/ModbusSSIHFB/crypto-material/config_solo/crypto-config/peerOrganizations/org2.example.com/users/User1@org2.example.com/msp/keystore/73beefad9003c589064deb2128c4f0831ba8003f1233102cc52a188afd05fe61_sk'
 pathpubKey = '/home/santiago/ModbusSSIHFB/crypto-material/config_solo/crypto-config/peerOrganizations/org2.example.com/users/User1@org2.example.com/msp/signcerts/User1@org2.example.com-cert.pem'
 did, controller, pubKey, payload = generateDIDentity(
     pathwallet, pathpubKey, pathprivKey, 1, 0, 0, 0, 0)
 
-# 9. El administrador registra el didDoc for server belonging Org1 (Depende de que 5 se haya llevado a cabo)
+# 11. El administrador registra el didDoc for server belonging Org1 (Depende de que 5 se haya llevado a cabo)
 net_profile = '../../connection-profile/2org_2peer_solo/network.json'
 organization = 'org1.example.com'
 user = 'Admin'
--- app/administrator/hfbssisdk/src/hfbssi/entity.py
+++ app/administrator/hfbssisdk/src/hfbssi/entity.py
@@ -1,10 +1,39 @@
 import os
+import json
+import codecs
+import hashlib
 import asyncio
 from hfc.fabric import Client
+from cryptography.hazmat.primitives import hashes
+from hfbssisdk.src.hfbssi.diddocclient import didDocClient
+from hfbssisdk.src.hfbssi.diddocserver import didDocServer
+from hfbssisdk.src.hfbssi.diddocclient import didDocClientSigned
+from hfbssisdk.src.hfbssi.diddocserver import didDocServerSigned
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import padding
+from jwt.utils import base64url_decode, force_bytes, force_unicode
+from cryptography.hazmat.primitives.serialization import (
+    load_pem_private_key, load_pem_public_key, load_ssh_public_key)
 
 
-def payloadToRegisterEntity(path_priv_key, did_wallet_path):
-    return 1
+def payloadToRegisterEntity(path_priv_key):
+    # Load admin private key https://github.com/pyca/cryptography/blob/master/docs/hazmat/primitives/asymmetric/ec.rst
+    with open(path_priv_key, 'r') as ec_priv_file:
+        priv_eckey = load_pem_private_key(force_bytes(
+            ec_priv_file.read()), password=None, backend=default_backend())
+
+    # Public Key serialization without encryption: https://cryptography.io/en/latest/hazmat/primitives/asymmetric/rsa.html#key-serialization
+    public_key = priv_eckey.public_key()
+    public_key_pem = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM, format=serialization.PublicFormat.SubjectPublicKeyInfo)
+
+    # Create DID identifier for administrator
+    did_entity = "did:vtn:trustid:" + \
+        str(hashlib.sha256(public_key_pem).hexdigest()
+            )
+    return did_entity, public_key_pem
 
 
 def registerEntity(net_profile, organization, user, channel, peer, chaincode, function, arg1, arg2, arg3):
